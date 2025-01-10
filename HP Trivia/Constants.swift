7b88f4aef0cf8ca490002d7f2546d7ecedee9a35
John Rogers <14traits@users.noreply.github.com>
2025-01-09 18:22:32 -0600

    app init, ++ all assets, layed out main screen

---
+++ HP Trivia/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  HP Trivia
+//
+//  Created by John Rogers on 1/9/25.
+//
+
+import Foundation
+
+enum Constants {
+    static let hpFont = "PartyLetPlain"
+}
--- HP Trivia/ContentView.swift
+++ HP Trivia/ContentView.swift
@@ -9,77 +9,97 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+   
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+        GeometryReader { geo in
+            ZStack {
+                Image("hogwarts")
+                    .resizable()
+                    .frame(width: geo.size.width * 3, height: geo.size.height)
+                    .padding(.top, 3)
+                
+                VStack {
+                    VStack {
+                        Image(systemName: "bolt.fill")
+                            .font(.largeTitle)
+                            .imageScale(.large)
+                        Text("HP")
+                            .font(.custom(Constants.hpFont, size: 70))
+                            .padding(.bottom, -50)
+                        Text("Trivia")
+                            .font(.custom(Constants.hpFont, size: 60))
                     }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
+                    .padding(.top, 70)
+                    
+                    
+                    Spacer()
+                    
+                    VStack {
+                        Text("Recent Scores!")
+                            .font(.title2)
+                        
+                        Text("33")
+                        Text("27")
+                        Text("15")
+                        
                     }
+                    .font(.title3)
+                    .padding(.horizontal)
+                    .foregroundColor(.white)
+                    .background(Color.black.opacity(0.7))
+                    .cornerRadius(15)
+                                  
+                    Spacer()
+                    
+                    HStack {
+                        Spacer()
+                        Button {
+                        } label: {
+                            Image(systemName: "info.circle.fill")
+                                .font(.largeTitle)
+                                .foregroundColor(.white)
+                                .shadow(radius: 5)
+                        }
+                        Spacer()
+                        
+                        Button {
+                                // Start new game
+                        } label: {
+                            Text("Play")
+                                .font(.largeTitle)
+                                .foregroundColor(.white)
+                                .padding(.vertical, 7)
+                                .padding(.horizontal, 50)
+                                .background(Color.brown)
+                                .cornerRadius(7)
+                                .shadow(radius: 5)
+                        }
+                        
+                        Spacer()
+                        
+                        Button {
+                            // Show settings screen
+                            
+                        } label: {
+                            Image(systemName: "gearshape.fill")
+                                .font(.largeTitle)
+                                .foregroundColor(.white)
+                                .shadow(radius: 5)
+                            
+                        }
+                        
+                        Spacer()
+                    }
+                    .frame(width: geo.size.width)
+                    
+                    Spacer()
                 }
             }
-            Text("Select an item")
+            .frame(width: geo.size.width , height: geo.size.height)
         }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+        .ignoresSafeArea()
     }
 }
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 #Preview {
     ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
